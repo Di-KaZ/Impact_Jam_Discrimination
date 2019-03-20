bde2db3b15882e4d492081c2e25a2b621d6367eb
Robi-Lolin <robin.veyssiere@epitech.eu>
2019-03-20 16:31:20 +0100

    Multiple circles creation

---
+++ project/Assets/square_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square_script : MonoBehaviour
+{
+    public GameObject circle;
+    private float x = 0;
+    private float y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0;i < 100; i++) {
+            x = Random.Range(-10.0f, 10.0f);
+            y = Random.Range(-10.0f, 10.0f);
+            Instantiate(circle, new Vector2(x, y), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
